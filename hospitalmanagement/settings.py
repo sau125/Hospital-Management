4c1d59facd0ab4ad88bfb3174b34de67c32453b2
Saurabh <shuklasaurabh67@gmail.com>
2021-09-26 17:56:35 +0530

    updated

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -128,8 +128,7 @@ STATICFILES_DIRS=[STATIC_DIR,]
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 
 LOGIN_REDIRECT_URL='/afterlogin'
 
