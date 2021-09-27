e3232ea14e6edbc950b9f205754d45762a036ebd
Saurabh <shuklasaurabh67@gmail.com>
2021-09-28 01:21:25 +0530

    modified

--- hospitalmanagement/urls.py
+++ hospitalmanagement/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
 
     path('aboutus', views.aboutus_view),
     path('contactus', views.contactus_view),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 
     path('doctorclick', views.doctorclick_view),
