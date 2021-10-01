54c0ee69873631ce73b7b5cd01bcf9453d1d04bb
Saurabh <shuklasaurabh67@gmail.com>
2021-10-01 19:55:23 +0530

    modified

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 from pathlib import Path
 import os
-
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -172,3 +172,4 @@ EMAIL_RECEIVING_USER = ['to@gmail.com'] # email on which you will receive messag
 
 
 # Activate Django-Heroku.
+django_heroku.settings(locals())
